374c3baf72b30934fb5d8c7b215f7b1eb4121535
ROBLERO QUIROS ALEX JOAN <alex.roblero0656@uhispano.ac.cr>
2026-09-16 10:27:21 -0600

    Revert "feat: mobile TabBar navigation with minimal header"
    This reverts commit 7db5916af44277f023b3442d77905c622634a1d4.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,7 +9,6 @@ import Contact from './features/Contact/pages/Contact'
 import NotFound from './features/NotFound/pages/NotFound'
 import Footer from './common/components/Footer/Footer'
 import Header from './common/components/Header/Header'
-import TabBar from './common/components/TabBar/TabBar'
 
 function App() {
   useScrollTop()
@@ -34,7 +33,6 @@ function App() {
         </Routes>
       </main>
       {isKnownRoute && <Footer />}
-      {isKnownRoute && <TabBar />}
     </div>
   )
 }
